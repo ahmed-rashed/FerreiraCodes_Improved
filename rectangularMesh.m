6b54c9deb5d0081b519bd474e6dc0ad93168a543
Unknown <ahmed.m.rashed@gmail.com>
2018-12-16 10:05:22 +0200

    Implement Project1

--- rectangularMesh.m
+++ rectangularMesh.m
@@ -1,12 +1,22 @@
-function [nodeCoordinates, elementNodes]=rectangularMesh(L_x,L_y,N_elements_X,N_elements_Y)
+function [nodeCoordinates, elementNodes]=rectangularMesh(L_x,L_y,N_elements_x,N_elements_y)
 
-x_vec=linspace(0,L_x,N_elements_X);
-y_vec=linspace(0,L_y,N_elements_Y);
+N_elements=N_elements_x*N_elements_y;
+N_nodes_x=N_elements_x+1;
+N_nodes_y=N_elements_y+1;
+
+x_vec=linspace(0,L_x,N_nodes_x);
+y_vec=linspace(0,L_y,N_nodes_y);
 [x_mat,y_mat]=meshgrid(x_vec,y_vec);
 
-N_nodes=(N_elements_X+1)*(N_elements_Y+1);
-N_elements=N_elements_X*N_elements_Y;
 
 nodeCoordinates=[x_mat(:),y_mat(:)];
 elementNodes=nan(N_elements,4);
-=[]
+for ii=1:N_elements_x
+    iStart=(ii-1)*N_nodes_y+1;
+    ind_sw=iStart+(1:N_elements_y).'-1;
+    ind_nw=ind_sw+1;
+    ind_ne=ind_nw+N_nodes_y;
+    ind_se=ind_sw+N_nodes_y;
+    elementNodes((ii-1)*N_elements_y+(1:N_elements_y),:)=[ind_se ind_ne ind_nw ind_sw];
+end
+
